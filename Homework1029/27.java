7a0b2578d5b3c3d7c4723aa0c2d8a752290534b1
Chanism <chanism.cheng@gmail.com>
2015-11-04 19:55:38 +0800

    RemoveIntoFolder

---
+++ Homework1029/27.java
@@ -0,0 +1,12 @@
+public class Solution {  
+    public int removeElement(int[] A, int elem) {  
+        int startPosition = 0;  
+        for (int i = 0; i < A.length; i++) {  
+            if (A[i] != elem) {  
+                A[startPosition] = A[i];  
+                startPosition++;  
+            }  
+        }  
+        return startPosition;  
+    }  
+} 
