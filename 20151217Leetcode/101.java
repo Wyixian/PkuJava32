32e20f2ae7ccf03c5cf15ba2552ccc5ba76f1799
Chanism <chanism.cheng@gmail.com>
2015-12-17 23:49:26 +0800

    add20151217Homework

---
+++ 20151217Leetcode/101.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        } 
+        else {
+            return isSame(root.left, root.right);
+        }
+    }
+    private boolean isSame(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }  
+        if (left != null && right == null || left == null && right != null){
+            return false;
+        } 
+        else {
+            return left.val == right.val && isSame(left.left, right.right) && isSame(left.right, right.left);
+        }
+    }
+}
